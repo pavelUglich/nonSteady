d543f7919f7edc600143f0757c120d1510fc56a9
Павел Углич <puglich@inbox.ru>
2021-06-03 09:51:58 +0300

    1

--- Solver19/Source.cpp
+++ Solver19/Source.cpp
@@ -152,7 +152,7 @@ void plotTheWaveField(double step, const std::map<std::string,
 	std::ofstream stream(fileName);
 	stream << "\\begin{tikzpicture}[scale=1.5]\n";
 	stream << "\\begin{axis}[grid]\n";
-	for (auto item : vectors)
+	for (const auto& item : vectors)
 	{
 		addTheCurve(step, item.second, stream, item.first, a);
 	}
@@ -166,25 +166,24 @@ int main()
 {
 	setlocale(0, "");
 	Parameters::kind = FIRST;
-	auto t = 0.01;
-	double a = 1.0;
-	double b = 10.0;
-	double h = 0.1;
-	double eps = 0.001;
+	const auto t = 1.0;
+	const double b = 5.0;
+	const double h = 0.1;
+	double eps = 0.000001;
 
 	std::vector<std::complex<double>> vector;
+	std::vector<std::complex<double>> vector1;
 
-	for (; a < b; a += h)
+	for (double a = 0.0; a < b; a += h)
 	{
 		std::complex<double> alpha = { a,0 };
 		//std::complex<double> alpha = { a,1 };
 		//const auto value = evaluate_integral([=](double s) {return new_integrand(alpha, s, t); }, 0, 2 * pi / t);		
 		vector.push_back(back_integrands(alpha, t, 0.0001));
-		//vector.push_back(new_integrand(alpha, a, t));
-
+		vector1.push_back(back_integrand(alpha, t, eps));
 	}
 
-	plotTheWaveField(h, { { "black", vector } }, "1.txt");
+	plotTheWaveField(h, { { "black", vector }, { "red", vector1 } }, "1.txt");
 
 
 	system("pause");
@@ -214,8 +213,8 @@ std::complex<double> integrand(std::complex<double> alpha, std::complex<double> 
 			[=](double x, const std::vector<std::complex<double>>& v) {return 1 / mu(x) * v[1]; }, // правые части уравнений системы
 			[=](double x, const std::vector<std::complex<double>>& v) {return (rho(x) * s * s + alpha * alpha * mu(x)) * v[0]; }
 
-		},{ { 0,0 } },{ { 1, 1.0 } } }; // краевые условия
-	return bvp.solve()[0] / s * exp(s * t); // набор точек
+		},{ { 0,0 } },{ { 1, 1.0 } } }; // краевые условия*/
+	return bvp.solve()[0] / s; // набор точек
 }
 
 //Аналитическое выражение
@@ -226,28 +225,27 @@ std::complex<double> analytical(std::complex<double> alpha, std::complex<double>
 	const auto e = exp(gamma);
 	const auto sh = (e - 1.0 / e) / 2.0;
 	const auto ch = (e + 1.0 / e) / 2.0;
-	return  exp(s * t) * (1.0 / s) * sh / (gamma * ch);
+	return  exp(s * t) * (1.0 / s / s) * sh / (gamma * ch);
 }
 
 //Условные вычисления
 
 std::complex<double> back_integrands(std::complex<double> alpha, double t, double eps)
 {
-	std::complex<double> sm = { 0.0, 0.0 };
 	int i = 0;
-
-	std::complex<double> value = { 1, 0 };
-
-	while (value.real() * value.real() + value.imag() * value.imag() > eps * eps)
-	{
+	const auto e = exp(alpha);
+	const auto sh = (e - 1.0 / e) / 2.0;
+	const auto ch = (e + 1.0 / e) / 2.0;
+	std::complex<double> sm = sh / alpha / ch;	
+	std::complex<double> value;
+	do {
 		if (i >= 1000) break;
-		const auto tmp = (2.0 * i - 1) * pi / 2.0;
-		const auto sqr = sqrt(alpha * alpha + tmp * tmp);
-		value = 2.0 * sin(tmp) * sin(t * sqr) / sqr;
-		if (i % 2 != 0) value *= -1;
+		const auto tmp = (2.0 * i + 1) * pi / 2.0;
+		const auto sqr = alpha * alpha + tmp * tmp;
+		value = -2.0 * cos(sqrt(sqr) * t) / sqr;
 		i++;
 		sm += value;
-	}
+	} while (value.real() * value.real() + value.imag() * value.imag() > eps * eps);
 	return sm;
 }
 
@@ -258,22 +256,25 @@ std::complex<double> contour(double s, double zeta)
 
 std::complex<double> new_integrand(std::complex<double> alpha, double s, double t)
 {
-	const std::complex<double> point = contour(s, 0.1);
-	return integrand(alpha, point, t);
+	const std::complex<double> I = { 0,1 };
+	const std::complex<double> point = contour(s, 0.5);
+	return integrand(alpha, point, t) * exp(0.5) * (cos(s) + I * sin(s)) * I;
 }
 
-std::complex<double> back_integrand(std::complex<double> alpha, /*std::complex<double> s,*/ double t, double eps)
+std::complex<double> back_integrand(std::complex<double> alpha, double t, double eps)
 {
 	std::complex<double> sum = { 0.0, 0.0 };
 	const std::complex<double> denumerator = { 0, 2 * pi };
-	int i = 0;
-	std::complex<double> value = evaluate_integral([=](double s) {return new_integrand(alpha, s, t); }, 0, 2 * pi / t);
+	int i = 1;
+	std::complex<double> value = evaluate_integral([=](double s) {return new_integrand(alpha, s, t); }, -pi / t, pi / t);
 	while (value.real() * value.real() + value.imag() * value.imag() > eps * eps)
 	{
 		sum += value;
-		i++;
 		if (i >= 1000) break;
-		value = evaluate_integral([=](double s) {return new_integrand(alpha, s, t); }, i * 2 * pi / t, (i + 1) * 2 * pi / t);
+		auto x = evaluate_integral([=](double s) {return new_integrand(alpha, s, t); }, pi * i / t, pi * (i + 2) / t);
+		auto y = evaluate_integral([=](double s) {return new_integrand(alpha, s, t); }, -pi * (i + 2) / t, -pi * i / t);
+		value = x + y;
+		i+=2;
 	}
 	return sum / denumerator;
 }
